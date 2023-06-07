83968068320cfe7af3cba7556dbc378735a5b0bf
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2023-06-07 10:34:57 -0300

    filtro react

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,6 +1,7 @@
 import Menu from "../components/Menu/Menu";
 import Cartao from "../components/Cartao/Cartao";
 import dados from "../data/informacoes.json";
+import { useState } from "react";
 
 /*const dados = [
     {
@@ -15,26 +16,35 @@ import dados from "../data/informacoes.json";
     }
 ];*/
 
-const Sobre = () => (
-    <>
-    <Menu />
-    <h1>Sobre</h1>
-    <p>Meu novo teste</p>
-    {
-        dados.map(
-            (ele, ind) => (
-               <Cartao
-                    key={ind}
-                    simb={ele.simbolo}
-                    descri={ele.descricao}
-                    data={ele.data}
-                    cor={ele.cor}
-               /> 
-            )
-        )
+const Sobre = () => {
+    const [info, setInfo] = useState(dados);
+
+    const filtra = (entrada) =>{
+        setInfo(dados.filter( (e) => e.descricao.includes(entrada)));
     }
-    </>
-);
+
+    return (
+        <>
+        <Menu />
+        <h1>Sobre</h1>
+        <p>Meu novo teste</p>
+        <input type='text' onChange={(e) => filtra(e.target.value)}/>
+        {
+            info.map(
+                (ele, ind) => (
+                <Cartao
+                        key={ind}
+                        simb={ele.simbolo}
+                        descri={ele.descricao}
+                        data={ele.data}
+                        cor={ele.cor}
+                /> 
+                )
+            )
+        }
+        </>
+    )
+};
 
 
 export default Sobre;
